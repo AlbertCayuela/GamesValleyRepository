73200501053709adf90e3b625141e6078ef92b39
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-04 20:25:01 +0200

    theme + bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 //*-----------------------------MAIN--------------------------------------------
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(GamesValley());
 }
 
-class MyApp extends StatelessWidget {
+class GamesValley extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GamesValley',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'OpenSans',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Body(),
-    );
-  }
-}
-
-class Body extends StatelessWidget {
-  const Body({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("GamesValley"),
-      ),
-      body: StreamBuilder(
-        stream: db.collection('users').doc('jbond').snapshots(),
-        builder: (context, snapshot) {
-          final doc = snapshot.data;
-          return Center(
-            child: Text('${doc['first_name']} ${doc['last_name']}'),
-          );
-        },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GamesValley'),      
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Offers'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          ),
       ),
     );
   }
 }
+

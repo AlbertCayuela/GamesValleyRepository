f541a345541611a48f27fda862eb8940992a1ec5
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-12 09:51:00 +0200

    hiding keyboard when tapping os signup screen

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -7,68 +7,72 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 100),
-              child: Text(
-                'GamesValley',
-                style: TextStyle(
-                    color: Colors.deepPurple,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.mail),
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: 'Username',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                obscureText: true, //todo show-hide password
-                decoration: InputDecoration(
-                  icon: Icon(Icons.vpn_key),
-                  labelText: 'Password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ),
-            OutlinedButton(
-                onPressed: null,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 100),
                 child: Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.deepPurple),
+                  'GamesValley',
+                  style: TextStyle(
+                      color: Colors.deepPurple,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
                 ),
-                style: OutlinedButton.styleFrom(
-                    primary: Colors.deepPurple,
-                    side: BorderSide(color: Colors.deepPurple))),
-            Text('or'),
-            OutlinedButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text('Login', style: TextStyle(color: Colors.blueGrey))),
-          ]),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.mail),
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.person),
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  obscureText: true, //todo show-hide password
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: null,
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      primary: Colors.deepPurple,
+                      side: BorderSide(color: Colors.deepPurple))),
+              Text('or'),
+              OutlinedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child:
+                      Text('Login', style: TextStyle(color: Colors.blueGrey))),
+            ]),
+          ),
         ),
       ),
     );

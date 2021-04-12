486961a9d1abfa035e7cdc3fccce64b22bb4da6c
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-12 09:36:47 +0200

    fixed login overflow + signup navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:games_valley/screens/base.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:games_valley/screens/login.dart';
+import 'package:games_valley/screens/profile.dart';
 import 'package:games_valley/screens/signup.dart';
 
 //*-----------------------------MAIN--------------------------------------------
@@ -20,7 +22,13 @@ class GamesValley extends StatelessWidget {
         fontFamily: 'OpenSans',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SignUpScreen(),
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/offers': (context) => LoginScreen(),
+        '/profile': (context) => ProfileScreen(),
+      },
+      home: LoginScreen(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,62 +10,67 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 100),
-            child: Text(
-              'GamesValley',
-              style: TextStyle(
-                  color: Colors.deepPurple,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.mail),
-                labelText: 'Email',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              obscureText: true, //todo show-hide password
-              decoration: InputDecoration(
-                icon: Icon(Icons.vpn_key),
-                labelText: 'Password',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          OutlinedButton(
-              onPressed: null,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 100),
               child: Text(
-                'Login',
-                style: TextStyle(color: Colors.deepPurple),
+                'GamesValley',
+                style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
               ),
-              style: OutlinedButton.styleFrom(
-                  primary: Colors.deepPurple,
-                  side: BorderSide(color: Colors.deepPurple))),
-          Text('or'),
-          OutlinedButton(onPressed: null, child: Text('Sign up')),
-          Expanded(
-              child: Align(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Forgot password?'),
             ),
-            alignment: Alignment.bottomCenter,
-          )),
-        ]),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(Icons.mail),
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true, //todo show-hide password
+                decoration: InputDecoration(
+                  icon: Icon(Icons.vpn_key),
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ),
+            Text('Forgot password?'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: OutlinedButton(
+                  onPressed: null,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                      primary: Colors.deepPurple,
+                      side: BorderSide(color: Colors.deepPurple))),
+            ),
+            Text('or'),
+            OutlinedButton(
+              child: Text(
+                'Sign up',
+                style: TextStyle(color: Colors.blueGrey),
+              ),
+              onPressed: () =>
+                  Navigator.pushReplacementNamed(context, '/signup'),
+            ),
+          ]),
+        ),
       ),
     );
   }

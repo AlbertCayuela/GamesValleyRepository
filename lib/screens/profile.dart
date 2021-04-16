414296350a02b0ae7864e7d74a5d2256ce2b3cd1
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-16 19:28:52 +0200

    working on profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -46,19 +46,120 @@ class ProfileScreenWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(context.read<UserRepository>().getUserEmail()),
-          RaisedButton(
-            onPressed: () {
-              context.read<UserRepository>().singOut();
-              Navigator.popAndPushNamed(context, '/authwrapper');
-            },
-            child: Text('Sing out'),
+      body: SafeArea(
+        child: Container(
+          color: Colors.grey[200],
+          child: Padding(
+            padding: const EdgeInsets.all(4),
+            child: Column(
+              children: [
+                MainInfoWidget(user: user),
+                SizedBox(height: 8),
+                Container(
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(7)),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Work Experience:',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          Text(user.username),
-          Text(user.uid),
-          Text(user.email),
+        ),
+      ),
+    );
+  }
+}
+
+class MainInfoWidget extends StatelessWidget {
+  const MainInfoWidget({
+    Key key,
+    @required this.user,
+  }) : super(key: key);
+
+  final UserInfo user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(7),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 60,
+            backgroundImage: NetworkImage(
+                'https://media.redadn.es/imagenes/otros-aticulos_308786.jpg'),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Margarita Robles',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(
+                    Icons.perm_identity,
+                    color: Colors.blueGrey,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    user.username,
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 15),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email_outlined,
+                    color: Colors.blueGrey,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    user.email,
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 15),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(
+                    Icons.phone_outlined,
+                    color: Colors.blueGrey,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    '+34 666 66 66 66',
+                    style: TextStyle(color: Colors.blueGrey, fontSize: 15),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );

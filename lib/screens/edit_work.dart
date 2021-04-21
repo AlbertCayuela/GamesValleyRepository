6620c3e799649862a927d2e1f840f194e00e5826
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-21 18:47:44 +0200

    edit work redistribution

--- lib/screens/edit_work.dart
+++ lib/screens/edit_work.dart
@@ -12,149 +12,185 @@ class _EditWorkScreenState extends State<EditWorkScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    super.initState(); 
+    super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit your work experience', style: TextStyle(fontWeight: FontWeight.bold),), ),
-      body: 
-          Container(
-          color: Colors.grey[200],
-          child: Padding(
-            padding: const EdgeInsets.all(4),
+      appBar: AppBar(
+        title: Text(
+          'Edit your work experience',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(10),
             child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Container(
-                    padding: EdgeInsets.all(7),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(7),                
-                    ),
-                    child: Row(        
-                      children: [
-                        Expanded(
-                            child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Add experience', style: TextStyle(fontSize: 20),),
-                              SizedBox(height: 5),
-                              TextField(
-                                decoration: InputDecoration(
-                                //icon: Icon(Icons.mail),
-                                labelText: 'Job',
-                                border: OutlineInputBorder(
-                                 borderRadius: BorderRadius.circular(10)),
-                                 ),
-                              ),
-                              SizedBox(height: 5),
-                              TextField(
-                                decoration: InputDecoration(
-                                //icon: Icon(Icons.mail),
-                                labelText: 'Company',
-                                border: OutlineInputBorder(
-                                 borderRadius: BorderRadius.circular(10)),
-                                 ),
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                children: [
-                                  DropdownButton(
-                                    value: startingMonth,
-                                    hint: Text('Starting month'),
-                                    items: [
-                                          DropdownMenuItem(child: Text('January'), value: 'January',),
-                                          DropdownMenuItem(child: Text('February'), value: 'February',),
-                                          DropdownMenuItem(child: Text('March'), value: 'March'),
-                                          DropdownMenuItem(child: Text('April'), value: 'April',),
-                                          DropdownMenuItem(child: Text('May'), value: 'May',),
-                                          DropdownMenuItem(child: Text('June'), value: 'June',),
-                                          DropdownMenuItem(child: Text('July'), value: 'July',),
-                                          DropdownMenuItem(child: Text('August'), value: 'August'),
-                                          DropdownMenuItem(child: Text('September'), value: 'September',),
-                                          DropdownMenuItem(child: Text('October'), value: 'October',),
-                                          DropdownMenuItem(child: Text('November'), value: 'November',),
-                                          DropdownMenuItem(child: Text('December'), value: 'December',),
-                                    ],
-                                    onChanged: (value){
-                                      setState(() {
-                                        startingMonth = value;
-                                      });
-                                    },
-                                  ),
-                                  SizedBox(width: 5),
-                                  Expanded(
-                                    
-                                     child: TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: InputDecoration(
-                                      
-                                     labelText: 'Starting year',
-                                     border: OutlineInputBorder(
-                                     borderRadius: BorderRadius.circular(10)),
-                                 ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 5),
-                              Row(
-                                children: [
-                                  DropdownButton(
-                                    value: finishMonth,
-                                    hint: Text('Finishing month'),
-                                    items: [
-                                          DropdownMenuItem(child: Text('January'), value: 'January',),
-                                          DropdownMenuItem(child: Text('February'), value: 'February',),
-                                          DropdownMenuItem(child: Text('March'), value: 'March'),
-                                          DropdownMenuItem(child: Text('April'), value: 'April',),
-                                          DropdownMenuItem(child: Text('May'), value: 'May',),
-                                          DropdownMenuItem(child: Text('June'), value: 'June',),
-                                          DropdownMenuItem(child: Text('July'), value: 'July',),
-                                          DropdownMenuItem(child: Text('August'), value: 'August'),
-                                          DropdownMenuItem(child: Text('September'), value: 'September',),
-                                          DropdownMenuItem(child: Text('October'), value: 'October',),
-                                          DropdownMenuItem(child: Text('November'), value: 'November',),
-                                          DropdownMenuItem(child: Text('December'), value: 'December',),
-                                    ],
-                                    onChanged: (value){
-                                      setState(() {
-                                        finishMonth = value;
-                                      });
-                                    },
-                                  ),
-                                  SizedBox(width: 5),
-                                  Expanded(
-                                    child: TextField(
-                                      keyboardType: TextInputType.number,
-                                      decoration: InputDecoration(
-                                      
-                                     labelText: 'Finish year',
-                                     border: OutlineInputBorder(
-                                     borderRadius: BorderRadius.circular(10)),
-                                 ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ]
-                          ),
-                        ),
-                      ],
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Add experience',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      //icon: Icon(Icons.mail),
+                      labelText: 'Job',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
                   ),
-                ),
-                
-            
-              ],
-              
-            ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      //icon: Icon(Icons.mail),
+                      labelText: 'Company',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  DropdownButton(
+                    isExpanded: true,
+                    value: startingMonth,
+                    hint: Text('Starting month'),
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('January'),
+                        value: 'January',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('February'),
+                        value: 'February',
+                      ),
+                      DropdownMenuItem(child: Text('March'), value: 'March'),
+                      DropdownMenuItem(
+                        child: Text('April'),
+                        value: 'April',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('May'),
+                        value: 'May',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('June'),
+                        value: 'June',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('July'),
+                        value: 'July',
+                      ),
+                      DropdownMenuItem(child: Text('August'), value: 'August'),
+                      DropdownMenuItem(
+                        child: Text('September'),
+                        value: 'September',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('October'),
+                        value: 'October',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('November'),
+                        value: 'November',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('December'),
+                        value: 'December',
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        startingMonth = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Starting year',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  DropdownButton(
+                    isExpanded: true,
+                    value: finishMonth,
+                    hint: Text('Finishing month'),
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('January'),
+                        value: 'January',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('February'),
+                        value: 'February',
+                      ),
+                      DropdownMenuItem(child: Text('March'), value: 'March'),
+                      DropdownMenuItem(
+                        child: Text('April'),
+                        value: 'April',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('May'),
+                        value: 'May',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('June'),
+                        value: 'June',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('July'),
+                        value: 'July',
+                      ),
+                      DropdownMenuItem(child: Text('August'), value: 'August'),
+                      DropdownMenuItem(
+                        child: Text('September'),
+                        value: 'September',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('October'),
+                        value: 'October',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('November'),
+                        value: 'November',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('December'),
+                        value: 'December',
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        finishMonth = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Finish year',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text('Add experience')),
+                ]),
           ),
-          
         ),
-      
+      ),
     );
   }
 }

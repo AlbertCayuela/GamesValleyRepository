837b5b05f2d2e76211b3c5428655ca6a9d6af9bf
AlbertCayuela <albertcayuela16@gmail.com>
2021-04-24 16:02:27 +0200

    languages edit base done

--- lib/screens/profile/languages_edit/languages_edit.dart
+++ lib/screens/profile/languages_edit/languages_edit.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/material.dart';
 
-class EditLanguagesScreen extends StatelessWidget {
+class EditLanguagesScreen extends StatefulWidget {
+  @override
+  _EditLanguagesScreenState createState() => _EditLanguagesScreenState();
+}
+
+class _EditLanguagesScreenState extends State<EditLanguagesScreen> {
+  String level;
+  TextEditingController _languageController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Edit your languages')),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    'Add language',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    controller: _languageController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a language';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Language',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  DropdownButtonFormField(
+                    isExpanded: true,
+                    value: level,
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select your language level';
+                      }
+                      return null;
+                    },
+                    hint: Text('Level'),
+                    items: [
+                      DropdownMenuItem(
+                        child: Text('Native'),
+                        value: 'Native',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Proficient'),
+                        value: 'Proficient',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Medium'),
+                        value: 'Medium',
+                      ),
+                      DropdownMenuItem(
+                        child: Text('Basic'),
+                        value: 'Basic',
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        level = value;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        print('all ok!');
+                      } else {
+                        print('something wrong!');
+                      }
+                    },
+                    child: Text('Add language'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

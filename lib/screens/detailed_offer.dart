221ce62cd7ebf3b5f937e49fbb533d56528a8597
AlbertCayuela <albertcayuela16@gmail.com>
2021-05-16 14:16:48 +0200

    base detailed offer done

--- lib/screens/detailed_offer.dart
+++ lib/screens/detailed_offer.dart
@@ -17,90 +17,164 @@ class DetailedOfferScreen extends StatelessWidget {
           color: Colors.grey,
           child: Padding(
             padding: const EdgeInsets.all(5.0),
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(7),
-                color: Colors.white,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flex(direction: Axis.horizontal),
-                  Container(
-                    alignment: Alignment.center,
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage: offer[3] != null && offer[3] != ''
-                              ? NetworkImage(offer[3])
-                              : null,
-                        ),
-                        Text(offer[4],
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 22)),
-                      ],
-                    ),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: Colors.white,
                   ),
-                  Text(
-                    offer[1],
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.deepPurple,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.place,
-                            color: Colors.blueGrey,
-                            size: 17,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            offer[5],
-                            style: TextStyle(color: Colors.blueGrey),
-                          ),
-                        ],
+                      Flex(direction: Axis.horizontal),
+                      Container(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundImage:
+                                  offer[3] != null && offer[3] != ''
+                                      ? NetworkImage(offer[3])
+                                      : null,
+                            ),
+                            Text(offer[4],
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22)),
+                          ],
+                        ),
                       ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.monetization_on_rounded,
-                            color: Colors.blueGrey,
-                            size: 17,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            offer[7],
-                            style: TextStyle(color: Colors.blueGrey),
-                          ),
-                        ],
+                      Text(
+                        offer[1],
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold),
                       ),
+                      SizedBox(height: 10),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Icon(
-                            Icons.schedule,
-                            color: Colors.blueGrey,
-                            size: 17,
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.place,
+                                color: Colors.blueGrey,
+                                size: 17,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                offer[5],
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                            ],
                           ),
-                          SizedBox(width: 5),
-                          Text(
-                            offer[8],
-                            style: TextStyle(color: Colors.blueGrey),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.monetization_on_rounded,
+                                color: Colors.blueGrey,
+                                size: 17,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                offer[7],
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.schedule,
+                                color: Colors.blueGrey,
+                                size: 17,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                offer[8],
+                                style: TextStyle(color: Colors.blueGrey),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flex(
+                        direction: Axis.horizontal,
+                      ),
+                      Text(
+                        'Requierements',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      SizedBox(height: 5),
+                      Text(offer[9]),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flex(
+                        direction: Axis.horizontal,
+                      ),
+                      Text(
+                        'Worker duties',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      SizedBox(height: 5),
+                      Text(offer[10]),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flex(
+                        direction: Axis.horizontal,
+                      ),
+                      Text(
+                        'Extra information',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      SizedBox(height: 5),
+                      Text(offer[11]),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ));

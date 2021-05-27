4dce10c76f7609afe54d8602b166654aa7952fdf
AlbertCayuela <albertcayuela16@gmail.com>
2021-05-27 20:13:43 +0200

    making profile screen

--- lib/screens/company/company_cv_profile.dart
+++ lib/screens/company/company_cv_profile.dart
@@ -8,6 +8,36 @@ class CompanyCVProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: user.profileImageUrl != ''
+                      ? NetworkImage(user.profileImageUrl)
+                      : null,
+                  child: user.profileImageUrl != '' ? null : Icon(Icons.person),
+                ),
+                Text(
+                  user.name + user.surname,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(user.email),
+                Text(user.phone),
+                Divider(),
+                Column(
+                  children: [Text('Work experience')],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

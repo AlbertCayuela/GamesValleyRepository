9be0f91e81da95a1b8401720e3ce54919bfef976
AlbertCayuela <albertcayuela16@gmail.com>
2021-05-27 16:20:21 +0200

    starting application by profile

---
+++ lib/screens/company/company_cv_profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:games_valley/repositories/user_repository.dart';
+
+class CompanyCVProfileScreen extends StatelessWidget {
+  final UserInformation user;
+  CompanyCVProfileScreen(this.user);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}

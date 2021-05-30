88717bfe68ac843930bcc333beaa26914381cb6d
AlbertCayuela <albertcayuela16@gmail.com>
2021-05-30 11:07:06 +0200

    starting languages delete

---
+++ lib/screens/profile/languages_edit/languages_delete.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LanguageDeleteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit you languages'),
+        centerTitle: true,
+      ),
+    );
+  }
+}

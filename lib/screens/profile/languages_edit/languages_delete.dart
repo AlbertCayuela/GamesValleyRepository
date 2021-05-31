1b8ab40f092769218a78e49351aec8eefa86be68
AlbertCayuela <albertcayuela16@gmail.com>
2021-05-31 17:54:52 +0200

    working on delete languages

--- lib/screens/profile/languages_edit/languages_delete.dart
+++ lib/screens/profile/languages_edit/languages_delete.dart
@@ -6,19 +6,47 @@ class LanguageDeleteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Edit you languages',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text(
+            'Edit you languages',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Column(children: [
-        for (int i = 0; i < languages.length; i++)
-          Container(
-            child: Row(children: []),
-          )
-      ]),
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            color: Colors.grey,
+            child: Column(children: [
+              for (int i = 0; i < languages.length; i++)
+                Container(
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(languages[i][0]),
+                        Text(
+                          ' - ',
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                        Text(languages[i][1],
+                            style: TextStyle(color: Colors.blueGrey)),
+                        Expanded(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                                child: Icon(Icons.clear, color: Colors.red),
+                                onPressed: () {}),
+                          ),
+                        )
+                      ],
+                    ))
+            ]),
+          ),
+        ));
   }
 }

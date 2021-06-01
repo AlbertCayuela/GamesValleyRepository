a9c4517affed1408b3c0d84516882f7ee63fbe92
AlbertCayuela <albertcayuela16@gmail.com>
2021-06-01 19:31:13 +0200

    fixed expanded text on languages

--- lib/screens/profile/languages_edit/languages_edit.dart
+++ lib/screens/profile/languages_edit/languages_edit.dart
@@ -15,7 +15,7 @@ class _LanguageEditScreenState extends State<LanguageEditScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.grey[200],
         appBar: AppBar(
           title: Text(
             'Edit you languages',
@@ -24,88 +24,81 @@ class _LanguageEditScreenState extends State<LanguageEditScreen> {
           centerTitle: true,
         ),
         body: SingleChildScrollView(
-          child: Container(
-            color: Colors.grey,
-            child: Column(children: [
-              for (int i = 0; i < widget.languages.length; i++)
-                Container(
-                    margin: EdgeInsets.all(5),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      children: [
-                        Text(widget.languages[i][0]),
-                        Text(
-                          ' - ',
-                          style: TextStyle(color: Colors.blueGrey),
-                        ),
-                        Text(widget.languages[i][1],
+          child: Column(children: [
+            for (int i = 0; i < widget.languages.length; i++)
+              Container(
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(widget.languages[i][0]),
+                      Text(
+                        ' - ',
+                        style: TextStyle(color: Colors.blueGrey),
+                      ),
+                      Expanded(
+                        child: Text(widget.languages[i][1],
                             style: TextStyle(color: Colors.blueGrey)),
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: TextButton(
-                                child: Icon(Icons.clear, color: Colors.red),
-                                onPressed: () {
-                                  showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          content: Text(
-                                              'Do you want to delete this language?'),
-                                          actions: [
-                                            TextButton(
-                                                onPressed: () async {
-                                                  await context
-                                                      .read<UserRepository>()
-                                                      .updateLanguages(
-                                                          this.widget.languages,
-                                                          i)
-                                                      .then((_) async {
-                                                    await context
-                                                        .read<UserRepository>()
-                                                        .getLanguages();
-                                                  });
-                                                  Navigator.pop(context);
-                                                },
-                                                child: Text('Yes')),
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.pop(context);
-                                              },
-                                              child: Text('Cancel'),
-                                            )
-                                          ],
-                                        );
-                                      });
-                                }),
-                          ),
-                        )
-                      ],
-                    )),
-              Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                    child: Text('Add a new language'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  CreateLanguageScreen())).then((languages) {
-                        if (languages != null) {
-                          setState(() {
-                            this.widget.languages = languages;
-                          });
-                        }
-                      });
-                    }),
-              ),
-            ]),
-          ),
+                      ),
+                      TextButton(
+                          child: Icon(Icons.clear, color: Colors.red),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    content: Text(
+                                        'Do you want to delete this language?'),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () async {
+                                            await context
+                                                .read<UserRepository>()
+                                                .updateLanguages(
+                                                    this.widget.languages, i)
+                                                .then((_) async {
+                                              await context
+                                                  .read<UserRepository>()
+                                                  .getLanguages();
+                                            });
+                                            Navigator.pop(context);
+                                          },
+                                          child: Text('Yes')),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('Cancel'),
+                                      )
+                                    ],
+                                  );
+                                });
+                          }),
+                    ],
+                  )),
+            Align(
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                  child: Text('Add a new language'),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                CreateLanguageScreen())).then((languages) {
+                      if (languages != null) {
+                        setState(() {
+                          this.widget.languages = languages;
+                        });
+                      }
+                    });
+                  }),
+            ),
+          ]),
         ));
   }
 }
